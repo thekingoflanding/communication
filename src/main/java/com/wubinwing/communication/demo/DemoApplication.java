83666e04d1eea522c39089f5b7ca78bdb4cad306
wubin <1904798759@qq.com>
2020-03-15 00:24:09 +0800

    第一次

---
+++ src/main/java/com/wubinwing/communication/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.wubinwing.communication.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
